51f5142e367e666524493c32fa0cb4322ca25acc
Agzamov1234 <144151566+Agzamov1234@users.noreply.github.com>
2024-01-31 12:00:26 +0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,41 @@
+#coding: utf-8
+#задача 1
+x = int(input("введите число x = "))
+y = 7*x+5
+print("ответ  y = ", y)
+
+#задача 2
+
+a = int(input("введите число а = "))
+p = a*4
+print("p =" , p)
+
+#задача 3
+r = int(input("введите число r = "))
+p = 3.14
+l = 2*d\4*p
+print ("s =", s)
+
+#4 
+d = int(input("введите число d = "))
+p = 3.14
+s = 2*d\4*p 
+print("s = ", s)
+
+#5
+a = int(input("введите катет1 d = "))
+b = int(input("введите катет2 d = "))
+g = 0.5*((a*a)+(b*b))
+print ("g =", g)
+
+#6
+a = int(input("введите число d = "))
+b = int(input("введите число d = "))
+p = int(input("введите число d = "))
+p 2a+b 
+print ("p =", p)
+
+
+
+
+
